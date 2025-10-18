a9f1539f356ead6874107dde3c9b4c956a5edaee
phili <philippo.sanecki@gmail.com>
2025-10-18 15:42:34 +0200

    [map-service] new: add initial service structure

---
+++ services/map-service/src/test/java/risk_radar/map_service/MapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package risk_radar.map_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
